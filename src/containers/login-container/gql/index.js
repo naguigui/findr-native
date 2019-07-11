17c47d093f5de6a94a696b2d581de6f0610d6976
Andrew Nguyen <andrewvonguyen24@hotmail.com>
2019-07-10 23:51:25 -0400

    removed global query/mutations

---
+++ src/containers/login-container/gql/index.js
@@ -0,0 +1,9 @@
+import gql from 'graphql-tag'
+
+export const LOGIN_MUTATION = gql`
+	mutation login($email: String!, $password: String!) {
+		login(input: { email: $email, password: $password }) {
+			accessToken
+		}
+	}
+`
