03597d790866030c7afdad5e9c39c6ab57971dcb
Andrew Nguyen <andrewvonguyen24@hotmail.com>
2019-07-14 23:21:29 -0400

    Update config for android and ios

--- config/development.js
+++ config/development.js
@@ -1,4 +1,12 @@
+import { Platform } from 'react-native'
+
+const IOS_PLATFORM = 'ios'
+
 export default {
-	BASE_API_URL: 'http://localhost:3000',
-	BASE_WS_API_URL: 'ws://localhost:3000',
+	BASE_API_URL:
+		Platform.OS === IOS_PLATFORM
+			? 'http://localhost:3000'
+			: 'http://10.0.2.2:3000',
+	BASE_WS_API_URL:
+		Platform.OS === IOS_PLATFORM ? 'ws://localhost:3000' : 'ws://10.0.2.2:3000',
 }
